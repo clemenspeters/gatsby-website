8352400597af33ab182fcab0481a67b37dc30efc
Clemens Peters <clemens.peters@honestfoodcompany.de>
2020-06-08 09:15:45 +0200

    :bug: Fix attempt for GTM.

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,9 +44,9 @@ module.exports = {
         defaultDataLayer: { platform: "clemenspeters" },
 
         // Specify optional GTM environment details.
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "YOUR_DATA_LAYER_NAME",
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
 
         // Name of the event that is triggered
         // on every Gatsby route change.
