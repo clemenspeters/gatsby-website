775774d1be933fae6829cd122bf3bb67b421bf8b
Clemens Peters <c.peters@conventic.com>
2025-09-01 23:35:00 +0200

    🔄 Update contentbot

--- src/components/seo.js
+++ src/components/seo.js
@@ -69,11 +69,7 @@ function SEO({ description, lang, meta, title }) {
         },
       ].concat(meta)}
     >
-      <script src="https://bot.conventic.com/cdn/t_2982a97dffa02919/cvt_script.1710836907212.min.js"></script>
-      <link
-        rel="stylesheet"
-        href="https://bot.conventic.com/cdn/t_2982a97dffa02919/cvt_sheet.1710836907212.min.css"
-      ></link>
+      <script src="https://contentbot.conventic.com/api/tenant/t_2982a97dffa02919/public/script/v1/chat-script/"></script>
     </Helmet>
   )
 }
